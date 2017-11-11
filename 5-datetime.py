a8a7a1b06f389b08dc48a61040af2ff19023db44
miracle2 <tami.ybp@gmail.com>
2017-11-11 13:43:01 -0500

    date parsing rounded to week

--- 5-datetime.py
+++ 5-datetime.py
@@ -2,4 +2,6 @@ from dateutil import parser
 import time
 
 date = parser.parse("Tuesday, October 17, 2017 at 6:17pm EDT")
-print(int(time.mktime(date.timetuple())))
+timestamp = int(time.mktime(date.timetuple()))
+week = int(timestamp/(60*60*24*7))*(60*60*24*7)
+print(week)
